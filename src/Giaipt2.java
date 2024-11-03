322584c3aebc45c120401c7f513e3aede54c86a6
JamesTruongg <trvantam2005@gmail.com>
2024-11-03 10:55:24 +0700

    Update Giaipt2.java

--- src/Giaipt2.java
+++ src/Giaipt2.java
@@ -1,28 +1,25 @@
 import java.util.Scanner;
 
-public class Giaipt2 {
+public class Giaipt1 {
     public static void main(String[] args) {
-
-        double a,b,c;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Nhap vao he so a: ");
-        a = sc.nextInt();
-        System.out.print("Nhap vao he so b: ");
-        b = sc.nextInt();
-        System.out.print("Nhap vao he so c: ");
-        c = sc.nextInt();
-        double D = -b * b - 4 * a * c;
-        if(D > 0){
-            double x1 = (-b + Math.sqrt(D)) / (2 * a);
-            double x2 = (-b - Math.sqrt(D)) / (2 * a);
-            System.out.printf("Phuong trinh co hai nghiem phan biet: x1 = "+ x1 + " x2 = "+x2);
-        }
-        else if(D == 0){
-            double x = -b / (2 * a);
-            System.out.printf("Phuong trinh co nghiem kep: x = "+ x);
-        }
-        else {
-            System.out.println("Phuong trinh vo nghiem");
-        }
+                double x,a,b; // Khai báo biến x, a, b;
+                Scanner sc = new Scanner(System.in); // Tạo đối tượng Scanner để đọc dữ liệu từ bàn phím
+                System.out.print("Nhap vao he so a: ");
+                a = sc.nextDouble(); // Nhập hệ số a từ bàn phím
+                System.out.print("Nhap vao he so b: ");
+                b = sc.nextDouble(); // Nhập hệ số b từ bàn phím
+                if(a == 0){          // Kiểm tra test1 a = 0 , nếu a == 0 thì chạy code vào test2
+                    if(b == 0){         // Nếu b == 0, thì phương trình vô số nghiệm
+                        System.out.println("Phuong trinh vo so nghiem!");
+                    }
+                    else {           // Ngược lại nếu b khác 0 thì phương trình vô nghiệm
+                        System.out.println("Phương trinh vo nghiem!");
+                    }
+                }
+                else{ // Nếu a khác 0 , thì phương trình có nghiệm x
+                    x = -b / a; // Tính nghiệm x theo ct x = -b / a;
+                    System.out.println("Nghiem cua phuong trinh "+ a + "x + " + b + " = 0 la: " + "x = " + x);
+                }
     }
 }
+
